fd0761ec7152c697b0fd7506d1c536f28842a785
Jacob1225 <carlone.jacob@gmail.com>
2022-09-14 12:17:00 -0400

    changed git ignore

--- server/microservices/get_stops.py
+++ server/microservices/get_stops.py
@@ -1,31 +1,32 @@
 import pandas as pd
-from ..libraries.distance import calculate_distance
+from libraries.distance import calculate_distance
 
 DEFAULT_RADIUS = 2
 
+
 def get_stops(request):
 
-    #TODO: add token authentication
+    # TODO: add token authentication
 
     payload = request.get_json()
 
-    curr_lat = payload.get('lat')
-    curr_lon = payload.get('lon')
-    radius = payload.get('radius')
+    curr_lat = payload.get("lat")
+    curr_lon = payload.get("lon")
+    radius = payload.get("radius")
 
     if not curr_lat or not curr_lon or not radius:
-        return ('missing latitude, longitude, or radius', 400)
+        return ("missing latitude, longitude, or radius", 400)
 
     try:
-        #load stops.csv from cloud storage
-        stops = pd.read_csv('gs://travel-buddy/static.stops.csv')
+        # load stops.csv from cloud storage
+        stops = pd.read_csv("gs://travel-buddy/static.stops.csv")
 
         for stop in stops.itertuples():
-            if (calculate_distance(curr_lat, stop.stop_lat, curr_lon, stop.stop_lon) <= radius):
-                print(stop) #for now get shop information
-            
-        return ('stops retrieved', 200)
-    
+            if calculate_distance(curr_lat, stop.stop_lat, curr_lon, stop.stop_lon) <= radius:
+                print(stop)  # for now get shop information
+
+        return ("stops retrieved", 200)
+
     except Exception as e:
         print(str(e))
-        return ('Error retriveing stops', 500)
+        return ("Error retriveing stops", 500)
