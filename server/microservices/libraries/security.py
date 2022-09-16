8864b237c969dff1f75532896a2bc8a09531b815
Jacob1225 <carlone.jacob@gmail.com>
2022-09-16 18:39:56 -0400

    clean security.py

---
+++ server/microservices/libraries/security.py
@@ -0,0 +1,23 @@
+import json, os
+from google.cloud import secretmanager
+
+
+class Authenticator:
+    def __init__(self):
+        self.project_id = os.environ["GCP_PROJECT"]
+
+    """
+        Method used to load secrets within microservices
+        returns a dictionary format for the requested secret
+        Ex: {'apiKey': 'xr434sdssx'}
+    """
+
+    def load_secrets(self, secret_name):
+        secret_client = secretmanager.SecretManagerServiceClient()
+        secret_request = {"name": f"projects/{self.project_id}/secrets/{secret_name}/versions/latest"}
+
+        response = secret_client.access_secret_version(secret_request)
+        secrets = response.payload.data.decode("UTF-8")
+        secrets_dict = json.loads(secrets)
+
+        return secrets_dict
